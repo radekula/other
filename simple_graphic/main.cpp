936e590e61af6b9c720f13417add1f62926b845d
Radosław Ulatowski <radekula@gmail.com>
2016-06-08 22:37:02 +0200

    More work

--- simple_graphic/main.cpp
+++ simple_graphic/main.cpp
@@ -2,135 +2,192 @@
 #include <math.h>
 #include <GL/glut.h>
 
-// ZMIENNE GLOBALNE INICJOWANE W FUNKCJI INIT
-
 // tytul okna
 char title[] = "RU";
 
+
 // limit klatek na sekunde 
 // (w celu zapewnienia stalej szybkosci animacji i obrotu)
-float fps_limit;
+float fps_limit = 60;
+
 
 // rozmiar okna
-unsigned int width;
-unsigned int height;
+unsigned int width = 400;
+unsigned int height = 400;
+
 
 // pozycja okna
-int position_x;
-int position_y;
+int position_x = 100;
+int position_y = 100;
+
+
+// parametry poczatkowe
+float distance = 30.0;
+float delta_distance = 0.5;
+
+float delta_angle[2] = {0.1, 0.1};
+
+float letters_width = 10.0;
+float letters_height = 20.0;
+float letters_depth = 4.0;
+float base_width = 4.0;
+
 
 // polozenie kamery
 float pos[3];
 float look[3];
 float side_up[3];
-float angle[2];	
-float distance;
+float angle[2];
 
-// dopuszczalna zmiana parametrow polozenia kamery na klatke
-float delta_distance;
-float delta_angle[2];
 
-float letters_width;
-float letters_height;
-float letters_depth;
+float cube[] = {
+        -0.5, -0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5,
+        
+        -0.5, -0.5, -0.5,
+        -0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, -0.5, -0.5,
+        
+        -0.5, -0.5, 0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, 0.5, 0.5,
+        
+        0.5, -0.5, 0.5,
+        0.5, -0.5, -0.5,
+        0.5, 0.5, -0.5,
+        0.5, 0.5, 0.5,
 
-float square[] = {
-		 0.0,  0.0, 0.0,
-         1.0,  0.0, 0.0,	
-         1.0,  1.0, 0.0,
-         0.0,  1.0, 0.0 
-        };
+        0.5, 0.5, 0.5,
+        0.5, 0.5, -0.5,
+        -0.5, 0.5, -0.5,
+        -0.5, 0.5, 0.5,
 
-unsigned short square_idx[] = {0, 1, 2, 3}; 
+        0.5, -0.5, 0.5,
+        0.5, -0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        -0.5, -0.5, 0.5
+    };
 
-float triangle[] = {
-		 0.0,  0.0, 0.0,
-         1.0,  0.0, 0.0,	
-         1.0,  1.0, 0.0
-        };
+float cube_colors[] = {
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
 
-unsigned short triangle_idx[] = {0, 1, 2}; 
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 0.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 1.0
+    };  
+
+unsigned short cube_idx[] = {
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15,
+        16, 17, 18, 19,
+        20, 21, 22, 23
+    };
 
 // zainicjowanie zmiennych dotyczacych okna
 void init_window()
 {
-	fps_limit = 60.0;
-	width = 400;
-	height = 300;
-	position_x = 100;
-	position_y = 100;
+    fps_limit = 60.0;
+    width = 400;
+    height = 300;
+    position_x = 100;
+    position_y = 100;
 };
 
 
 // zainicjowanie zmiennych dotyczacych kamery
 void init_camera()
 {
-	distance = 30.0;
-	delta_distance = 0.5;
-	angle[0] = 0;
-	angle[1] = 0;
+    angle[0] = 0;
+    angle[1] = 0;
 
-	delta_angle[0] = 0.1;
-	delta_angle[1] = 0.1;
+    look[0] = 0.0;
+    look[1] = 0.0;
+    look[2] = 0.0;
 
-	look[0] = 0.0;
-	look[1] = 0.0;
-	look[2] = 0.0;
-
-	side_up[0] = 0.0;
-	side_up[1] = 1.0;
-	side_up[2] = 0.0;
+    side_up[0] = 0.0;
+    side_up[1] = 1.0;
+    side_up[2] = 0.0;
 };
 
 
 // wyliczenie nowej pozycji kamery
 void camera_calculate_pos()
 {
-	pos[0] = sin(angle[0]) * distance;
-	pos[1] = 0;
-	pos[2] = cos(angle[0]) * distance;
+    pos[0] = sin(angle[0]) * distance;
+    pos[1] = 0;
+    pos[2] = cos(angle[0]) * distance;
 
-	pos[0] = pos[0] * cos(angle[1]);
-	pos[1] = sin(angle[1]) * distance;
-	pos[2] = pos[2] * cos(angle[1]);
+    pos[0] = pos[0] * cos(angle[1]);
+    pos[1] = sin(angle[1]) * distance;
+    pos[2] = pos[2] * cos(angle[1]);
 }
 
 
 // przesuniecie kamery w lewo
 void camera_left()
 {
-	angle[0] -= delta_angle[0];
-	camera_calculate_pos();
+    angle[0] -= delta_angle[0];
+    camera_calculate_pos();
 };
 
 
 // przesuniecie kamery w prawo
 void camera_right()
 {
-	angle[0] += delta_angle[0];
-	camera_calculate_pos();
+    angle[0] += delta_angle[0];
+    camera_calculate_pos();
 };
 
 
 // przesuniecie kamery w gore
 void camera_up()
 {
-	angle[1] += delta_angle[1];
-	if(angle[1] > 1.57)
-		angle[1] = 1.57;
+    angle[1] += delta_angle[1];
+    if(angle[1] > 1.57)
+        angle[1] = 1.57;
 
-	camera_calculate_pos();
+    camera_calculate_pos();
 };
 
 
 // przesuniecie kamery w dol
 void camera_down()
 {
-	angle[1] -= delta_angle[1];
-	if(angle[1] < -1.57)
-		angle[1] = -1.57;
+    angle[1] -= delta_angle[1];
+    if(angle[1] < -1.57)
+        angle[1] = -1.57;
 
-	camera_calculate_pos();
+    camera_calculate_pos();
 };
 
 
@@ -138,331 +195,221 @@ void camera_down()
 // przyblizenie kamery
 void camera_closer()
 {
-	distance -= delta_distance;
-	if(distance < 0.1)
-		distance = 0.1;
+    distance -= delta_distance;
+    if(distance < 0.1)
+        distance = 0.1;
 
-	camera_calculate_pos();
+    camera_calculate_pos();
 };
 
 
 // oddalenie kamery
 void camera_further()
 {
-	distance += delta_distance;
-	if(distance > 100)
-		distance = 100;
+    distance += delta_distance;
+    if(distance > 100)
+        distance = 100;
 
-	camera_calculate_pos();
+    camera_calculate_pos();
 };
 
 
 // funkcja obslugujaca zmiane rozmiaru okna
 void window_resize(int new_width, int new_height)
 {
-	width = new_width;
-	height = new_height;
+    width = new_width;
+    height = new_height;
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-	glViewport(0, 0, width, height);		
-	gluPerspective(90.0, (float) width / (float) height, 0.01, 1000.0);
+    glViewport(0, 0, width, height);        
+    gluPerspective(90.0, (float) width / (float) height, 0.01, 1000.0);
 };
 
 
 // funkcja obslugi klawiszy
 void normal_keys(unsigned char key, int x, int y)
 {
-	switch(key)
-	{
-		case '+':
-		case '=':
-			camera_closer();
-			break;
-		case '-':
-			camera_further();
-			break;
-	};
+    switch(key)
+    {
+        case '+':
+        case '=':
+            camera_closer();
+            break;
+        case '-':
+            camera_further();
+            break;
+    };
 };
 
 
 // funkcja obslugi klawiszy specjalnych
 void specjal_keys(int key, int x, int y)
 {
-	switch(key)
-	{
-		case GLUT_KEY_RIGHT:
-			camera_right();
-			break;
-		case GLUT_KEY_LEFT:
-			camera_left();
-			break;
-		case GLUT_KEY_UP:
-			camera_up();
-			break;
-		case GLUT_KEY_DOWN:
-			camera_down();
-			break;
-	};
+    switch(key)
+    {
+        case GLUT_KEY_RIGHT:
+            camera_right();
+            break;
+        case GLUT_KEY_LEFT:
+            camera_left();
+            break;
+        case GLUT_KEY_UP:
+            camera_up();
+            break;
+        case GLUT_KEY_DOWN:
+            camera_down();
+            break;
+    };
 };
 
 
-
-// rysowanie kwadratu z uzyciem tabeli wierzcholkow
-void draw_square()
+// funkcja rysujaca szescian
+void draw_cude()
 {
-	glVertexPointer(3, GL_FLOAT, 0, square);
-	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_SHORT, square_idx);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_SHORT, cube_idx);
 };
 
 
-// rysowanie trojkata z uzyciem tabeli wierzcholkow
-void draw_triangle()
+// funkcja rysujaca szescian z kolorami
+void draw_cude_with_colors()
 {
-	glVertexPointer(3, GL_FLOAT, 0, triangle);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, triangle_idx);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    
+    glVertexPointer(3, GL_FLOAT, 0, cube);
+    glColorPointer(3, GL_FLOAT, 0, cube_colors);
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_SHORT, cube_idx);
 };
 
 
-// funkcja rysujaca przod (tyl) litery R
-void draw_R_face()
-{
-	float w_size = letters_width * 0.25;
-	float h_size = letters_height * 0.125;
-	
-	glPushMatrix();
-		glTranslatef(0.0, 0.0, 0.0);
-		glScalef(w_size, letters_height, 1.0);
-		draw_square();
-	glPopMatrix();
 
-	glPushMatrix();
-		glTranslatef(w_size, letters_height, 0.0);
-		glRotatef(-90.0, 0.0, 0.0, 1.0);
-		glScalef(h_size, w_size * 2, 1.0);
-		draw_square();
-	glPopMatrix();
 
-	glPushMatrix();
-		glTranslatef(w_size * 3, letters_height, 0.0);
-		glRotatef(-90.0, 0.0, 0.0, 1.0);
-		glScalef(h_size, w_size, 1.0);
-		draw_triangle();
-	glPopMatrix();
 
-	glPushMatrix();
-		glTranslatef(w_size * 4, letters_height - h_size, 0.0);
-		glRotatef(180.0, 0.0, 0.0, 1.0);
-		glScalef(w_size, h_size * 3, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(w_size * 4, letters_height - h_size * 4, 0.0);
-		glRotatef(180.0, 0.0, 0.0, 1.0);
-		glScalef(w_size, h_size, 1.0);
-		draw_triangle();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(w_size, letters_height - h_size * 4, 0.0);
-		glRotatef(-90.0, 0.0, 0.0, 1.0);
-		glScalef(h_size, w_size * 2, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(w_size * 4, letters_height - h_size * 5, 0.0);
-		glRotatef(180.0, 0.0, 0.0, 1.0);
-		glScalef(w_size, h_size * 3, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(w_size * 3, letters_height - h_size * 4, 0.0);
-		glRotatef(-90.0, 0.0, 0.0, 1.0);
-		glScalef(w_size, h_size, 1.0);
-		draw_triangle();
-	glPopMatrix();
-
-};
-
-// funkcja rysujaca boki litery R
-void draw_R_side()
-{
-	return;
-// pionowe sciany
-	glPushMatrix();
-		glRotatef(-90.0, 0.0, 1.0, 0.0);
-		glScalef(4.0, 20.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(3.0, 0.0, 0.0);
-		glRotatef(-90.0, 0.0, 1.0, 0.0);
-		glScalef(4.0, 20.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(9.0, 0.0, 0.0);
-		glRotatef(-90.0, 0.0, 1.0, 0.0);
-		glScalef(4.0, 20.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(12.0, 11.0, 0.0);
-		glRotatef(-90.0, 0.0, 1.0, 0.0);
-		glScalef(4.0, 6.0, 1.0);
-		draw_square();
-	glPopMatrix();
-	
-	glPushMatrix();
-		glTranslatef(12.0, 0.0, 0.0);
-		glRotatef(-90.0, 0.0, 1.0, 0.0);
-		glScalef(4.0, 8.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-// poziome sciany
-	glPushMatrix();
-		glTranslatef(0.0, 20.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(9.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(0.0, 17.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(9.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(0.0, 11.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(9.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(0.0, 8.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(9.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(9.0, 0.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(3.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(0.0, 0.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glScalef(3.0, 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-	
-// skosne sciany
-	glPushMatrix();
-		glTranslatef(9.0, 20.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glRotatef(-45.0, 0.0, 1.0, 0.0);
-		glScalef(3.0 * sqrt(2), 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-	
-	glPushMatrix();
-		glTranslatef(9.0, 8.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glRotatef(45.0, 0.0, 1.0, 0.0);
-		glScalef(3.0 * sqrt(2), 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(9.0, 11.0, 0.0);
-		glRotatef(90.0, 1.0, 0.0, 0.0);
-		glRotatef(-45.0, 0.0, 1.0, 0.0);
-		glScalef(3.0 * sqrt(2), 4.0, 1.0);
-		draw_square();
-	glPopMatrix();
-};
-
-// rysowanie litery R
 void letter_R()
 {
-// przod litery
-	glPushMatrix();
-		glColor3f(1.0, 1.0, 0.0);
-		glTranslatef(0.0, 0.0, 2.0);
-		draw_R_face();
-	glPopMatrix();
-	
-// tyl litery
-	glPushMatrix();
-		glColor3f(0.0, 1.0, 0.0);
-		glTranslatef(0.0, 0.0, -2.0);
-		draw_R_face();
-	glPopMatrix();
+    glScalef(letters_width, letters_height, letters_depth);
 
-// boki litery
-	glPushMatrix();
-		glColor3f(0.0, 1.0, 1.0);
-		glTranslatef(0.0, 0.0, -2.0);
-		draw_R_side();
-	glPopMatrix();
+    glPushMatrix();
+        glTranslatef(-0.4, 0.0, 0.0);
+        glScalef(0.2, 1.0, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(0.0, 0.45, 0.0);
+        glScalef(0.6, 0.1, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(0.0, -0.05, 0.0);
+        glScalef(0.6, 0.1, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+    
+    glPushMatrix();
+        glTranslatef(0.4, 0.2, 0.0);
+        glScalef(0.2, 0.4, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+    
+    glPushMatrix();
+        glTranslatef(0.4, -0.3, 0.0);
+        glScalef(0.2, 0.4, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+};
+
+
+void letter_U()
+{
+    glScalef(letters_width, letters_height, letters_depth);
+
+    glPushMatrix();
+        glTranslatef(-0.4, 0.05, 0.0);
+        glScalef(0.2, 0.9, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(0.4, 0.05, 0.0);
+        glScalef(0.2, 0.9, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+    
+    glPushMatrix();
+        glTranslatef(0.0, -0.45, 0.0);
+        glScalef(0.6, 0.1, 1.0);
+        draw_cude_with_colors();
+    glPopMatrix();
+};
+
+
+void base()
+{
+    glPushMatrix();
+        draw_cude();
+    glPopMatrix();
 };
 
 
 void scena()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	
-	gluLookAt(pos[0], pos[1], pos[2],
-			  look[0], look[1], look[2],
-			  side_up[0], side_up[1], side_up[2]); 
-	
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    gluLookAt(pos[0], pos[1], pos[2],
+              look[0], look[1], look[2],
+              side_up[0], side_up[1], side_up[2]); 
+    
     glColor3f(0.0, 0.0, 0.0);
-	letter_R();
+
+    glPushMatrix();
+        glTranslatef(-6.0, 0.0, 0.0);
+        letter_R();
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslatef(6.0, 0.0, 0.0);
+        letter_U();
+    glPopMatrix();
+
+    glPushMatrix();
+        glColor3f(1.0, 0.0, 0.0);
+        glTranslatef(0.0, -(letters_height / 2 + 2), 0.0);
+        glScalef(letters_width * 3, base_width, letters_width + 2);
+        base();
+    glPopMatrix();
     
-/*    glTranslatef(5.0, 0, 0);
-	glColor3f(1.0, 0.0, 0.0);
-	kwadrat();*/
-    
-	glFlush();
-	glutSwapBuffers(); 
+    glFlush();
+    glutSwapBuffers(); 
 }
 
 
 
 void render_frame(int)
 {
-	clock_t start_frame;
-	clock_t end_frame;
-	clock_t frame_time;
-	
-	start_frame = clock();
-	
-	glutPostRedisplay();
-	
-	end_frame = clock();
+    clock_t start_frame;
+    clock_t end_frame;
+    clock_t frame_time;
+    
+    start_frame = clock();
+    
+    glutPostRedisplay();
+    
+    end_frame = clock();
 
-	int max_frame_time = 1000 / fps_limit;	
-	if(max_frame_time > frame_time)
-		glutTimerFunc(max_frame_time - (end_frame - start_frame), render_frame, 0);
-	else
-		glutTimerFunc(1, render_frame, 0);
+    int max_frame_time = 1000 / fps_limit;    
+    if(max_frame_time > frame_time)
+        glutTimerFunc(max_frame_time - (end_frame - start_frame), render_frame, 0);
+    else
+        glutTimerFunc(1, render_frame, 0);
 };
 
 
@@ -471,31 +418,25 @@ void render_frame(int)
 
 void init(int argc, char *argv[])
 {
-	letters_width = 16.0;
-	letters_height = 24.0;
-	letters_depth = 4.0;
-	
-	init_camera();
-	init_window();
-	camera_calculate_pos();
-	
-	glutInit(&argc, argv);  
+    init_camera();
+    init_window();
+    camera_calculate_pos();
+    
+    glutInit(&argc, argv);  
 
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
-	glutInitWindowSize(width, height);
-	glutInitWindowPosition(position_x, position_y);
-	
-	glutCreateWindow(title);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(position_x, position_y);
+    
+    glutCreateWindow(title);
 
-	glutDisplayFunc(scena);
-	glutReshapeFunc(window_resize);
-	glutKeyboardFunc(normal_keys);
-	glutSpecialFunc(specjal_keys);
-	
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glEnable(GL_DEPTH_TEST);
-
-	glEnableClientState(GL_VERTEX_ARRAY);
+    glutDisplayFunc(scena);
+    glutReshapeFunc(window_resize);
+    glutKeyboardFunc(normal_keys);
+    glutSpecialFunc(specjal_keys);
+    
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
 
     glutTimerFunc(1000 / fps_limit, render_frame, 0);
 };
@@ -503,6 +444,6 @@ void init(int argc, char *argv[])
 
 int main(int argc, char** argv)
 {
-	init(argc, argv);
-	glutMainLoop();
+    init(argc, argv);
+    glutMainLoop();
 }
